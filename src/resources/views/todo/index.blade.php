6b9f53f7c867e61d70c041efbdec84f5e70cf33a
SidiqFatuh187 <kmzbengbeng14@gmail.com>
2026-05-27 10:41:43 +0700

    fix: menambahkan type file agar bisa mengupload dan menampilkan file

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -187,6 +187,17 @@
                         </div>
                     </div>
 
+                    {{-- File Attachment --}}  
+                    @if($todo->file_path)
+                        <a href="{{ Storage::url($todo->file_path) }}" target="_blank"
+                            class="flex items-center gap-1 text-xs text-blue-500 hover:text-blue-600 transition-colors">
+                            <svg class="w-3.5 h-3.5 shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            {{ $todo->file_name }}
+                        </a>
+                    @endif
+
                     {{-- Actions --}}
                     <div class="flex items-center gap-1.5 shrink-0">
                         <a href="{{ route('todo.edit', $todo->id) }}"
