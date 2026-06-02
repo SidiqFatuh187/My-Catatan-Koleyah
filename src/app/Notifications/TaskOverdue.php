96d23a2e94221709f653ec1e1ddcdbaee8b75582
SidiqFatuh187 <kmzbengbeng14@gmail.com>
2026-06-02 08:23:52 +0700

    feat: Menambah table notification dan pesan notif

---
+++ src/app/Notifications/TaskOverdue.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Todo;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TaskOverdue extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toArray(object $notifiable): array
+    {
+       return [
+            'message' => "Task \"{$this->todo->title}\" sudah melewati deadline.",
+            'task_id' => $this->todo->id,
+            'type'    => 'overdue',
+        ];
+    }
+
+}
